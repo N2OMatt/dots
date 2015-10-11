e039aa8e27da0fa1a0968e6228efef0a5d3ea516
N2OMatt <n2o.matt@gmail.com>
2015-10-11 20:14:24 -0300

    Add some aliases. Add the google-search function.

--- osx/osx.sh
+++ osx/osx.sh
@@ -2,32 +2,37 @@
 ################################################################################
 ## Aliases                                                                    ##
 ################################################################################
-## ls 
+## ls
 #In OSX and Linux the ls are different.
 alias ls="ls -GFh" #Colorize | Classify | human sizes.
 alias la="ls -a"   #Print hidden.
 alias ll="ls -l"   #Long list.
-
-## mv 
-alias mv="mv -vi" #Make mv verbose and iteractive.
-
+## mv
+alias mv="mv -vi" #Make mv verbose and iterative.
 ## cp
 alias cp="cp -v" #Make cp verbose.
+## df
+alias df="df -H" #Human readable.
+## du
+alias du="du -ch" #Grand Total | Human readable.
 
 ## clang/g++
-alias g++="g++ -std=c++11" #C++11 by defaut.
+alias g++="g++ -std=c++11" #C++11 by default.
+
+## Google Chrome ##
+alias chrome="open -a \"Google Chrome\"";
 
 ################################################################################
 ## Prompt                                                                     ##
 ################################################################################
-#Makes my prompt be: 
+#Makes my prompt be:
 #   Amanda:Directory:n2omatt $
 #Where Amanda is random colored - Without the black.
 PS1="\[$(tput setaf $(( 1 + RANDOM % 7)) )\]\h\[$(tput sgr 0)\]:\W \u\$ "
 
 
 ################################################################################
-## Manual                                                                     ##
+## Functions                                                                  ##
 ################################################################################
 ## Saves a manual page to the current directory.
 ## Very same way of call man...
@@ -41,4 +46,11 @@ saveman-pdf() {
 saveman() {
     saveman-txt $1 $2
     saveman-pdf $1 $2
+}
+
+## Make a google search and opens the result on Chrome.
+google-search()
+{
+    #COWTODO: We should encode the $1 - C++ -> C%2B%2B
+    chrome "https://www.google.com/search?q=$1";
 }
