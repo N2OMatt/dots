2820e196d22d9825f5d0dcdbeb54731aab8b1ebd
n2omatt <n2o.matt@gmail.com>
2016-04-17 08:01:24 -0300

    - Add the file header. - Add separators. - Add comments. - Improve the google-search function.

--- linux/browser.sh
+++ linux/browser.sh
@@ -1,24 +1,38 @@
 ################################################################################
-## apt                                                                        ##
-## COWTODO: Add description                                                   ##
+## File    : browser.sh                                                       ##
+## Project : dots                                                             ##
+## License : GPLv3                                                            ##
+## Author  : n2omatt@amazingcow.com                                           ##
+##                                                                            ##
+## Description:                                                               ##
+## Define browser stuff for scripts and interactive shell usage.              ##
 ################################################################################
 
-## Alias ##
+################################################################################
+## Aliases                                                                    ##
+################################################################################
+# This will make very easy to open the browsers from command line.
 alias chrome="chromium-browser";
 alias browser="chrome";
 
 
-## Exports ##
+################################################################################
+## Exports                                                                    ##
+################################################################################
+# Export the name as a facility to any of my scripts to open my selected
+# browser, since it could not know about my aliases.
 export N2OBROWSER="chromium-browser";
 
 
-## Functions ##
+################################################################################
+## Functions                                                                  ##
+################################################################################
 ## Make a google search and opens the result on browser.
 google-search()
 {
     #Encode the all function arguments.
-    local URL=$(url-encode $@)
+    local url=$(url-encode $@);
 
     #Redirect the error output...
-    browser "https://www.google.com/search?q=$URL" 2> /dev/null;
+    browser "https://www.google.com/search?q=$url" 2>&1 1> /dev/null;
 }
