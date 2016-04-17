3f67f7905e6a1c56434e2b05826097a4e617d158
n2omatt <n2o.matt@gmail.com>
2016-04-17 08:24:01 -0300

    - Add the file header. - Add comments. - Add a lot of easy typing aliases.

--- linux/git.sh
+++ linux/git.sh
@@ -1,9 +1,25 @@
 ################################################################################
-## apt                                                                        ##
-## COWTODO: Add description                                                   ##
+## File    : git.sh                                                           ##
+## Project : dots                                                             ##
+## License : GPLv3                                                            ##
+## Author  : n2omatt@amazingcow.com                                           ##
+##                                                                            ##
+## Description:                                                               ##
+## Define aliases for easy the git.                                           ##
 ################################################################################
 
-## Alias ##
-
+################################################################################
+## Aliases                                                                    ##
+################################################################################
+#Make gitcheck available easily.
 alias gitcheck="python ~/.local/lib/python2.7/site-packages/gitcheck/gitcheck.py"
-alias git-push-tags="git push --tags"
+
+#Less typing FTW.
+alias ggui="git gui";
+alias gstatus="git status -s";
+alias glog="git log";
+alias gadd="git add";
+alias gcommit="git commit";
+alias gpush="git push --tags";
+alias gpull="git pull";
+
