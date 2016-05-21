f809a5189376004e5fb585ee91409b6c30c674ad
n2omatt <n2o.matt@gmail.com>
2016-05-21 19:19:30 -0300

    Add amazingcow stuff.

---
+++ linux/AmazingCow.sh
@@ -0,0 +1,9 @@
+################################################################################
+## Exports                                                                    ##
+################################################################################
+export AMAZING_COW_PROJECTS_DIRECTORY="/home/n2omatt/Documents/Projects/AmazingCow";
+
+connect_amazingcow_dot_com()
+{
+    ssh -p2222 amazi671@amazingcow.com
+}
