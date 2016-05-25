685a18020e7d5431b21978d1ce6c0ff6be2ac3ba
n2omatt <n2o.matt@gmail.com>
2016-05-25 03:51:56 -0300

    - Add function to turn youtube videos into mp3.   NOW I CAN DOWNLOAD VIDEO GAME SOUND TRACKS!!!!!

--- linux/youtube-dl.sh
+++ linux/youtube-dl.sh
@@ -9,3 +9,19 @@ youtube-dl-auto()
 {
     youtube-dl $1 && exit;
 }
+
+youtube-dl-mp3()
+{
+     # youtube-dl-mp3  https://www.youtube.com/watch?v=3wKGLMyOinw
+    local download_name=$(echo "$1" | sed s/"https:\/\/www\.youtube\.com\/watch?v="//g);
+    youtube-dl $1
+
+    local original_file=$(ls *$download_name*);
+    local target_file=$(echo $original_file | \
+                        sed s/\.mkv/\.mp3/g | \
+                        sed s/\.mp4/\.mp3/g);
+
+    ffmpeg -i "$original_file" -vn -c:a libmp3lame "$target_file";
+
+    rm "$original_file";
+}
