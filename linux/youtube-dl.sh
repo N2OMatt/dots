6e50094055f753a05c2f7e9e2a8c2819a5739149
n2omatt <n2o.matt@gmail.com>
2016-06-27 23:39:53 -0300

    Found a flag that do EXACTLY the same thing that we want.

--- linux/youtube-dl.sh
+++ linux/youtube-dl.sh
@@ -12,17 +12,5 @@ youtube-dl-auto()
 
 youtube-dl-mp3()
 {
-     # youtube-dl-mp3  https://www.youtube.com/watch?v=3wKGLMyOinw
-    local download_name=$(echo "$1" | sed s/"https:\/\/www\.youtube\.com\/watch?v="//g);
-    youtube-dl $1
-
-    local original_file=$(ls *$download_name*);
-    local target_file=$(echo $original_file  | \
-                        sed s/\.mkv/\.mp3/g  | \
-                        sed s/\.webm/\.mp3/g | \
-                        sed s/\.mp4/\.mp3/g);
-
-    ffmpeg -i "$original_file" -vn -c:a libmp3lame "$target_file";
-
-    rm "$original_file";
+    youtube-dl $1 --extract-audio --audio-format mp3 && exit
 }
