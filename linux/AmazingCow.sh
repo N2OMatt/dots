c0740f8deffcf1d3f82f704e3d0757bea4b1d5ce
n2omatt <n2o.matt@gmail.com>
2016-08-17 18:57:53 -0300

    - Move all connections function to a separated file.

--- linux/AmazingCow.sh
+++ linux/AmazingCow.sh
@@ -4,7 +4,3 @@
 export AMAZING_COW_PROJECTS_DIRECTORY="/home/$USER/Documents/Projects/AmazingCow";
 
 
-connect_amazingcow_dot_com()
-{
-    ssh -p2222 amazi671@amazingcow.com
-}
---
+++ linux/connections.sh
@@ -0,0 +1,14 @@
+connect_amazingcow_dot_com()
+{
+    ssh -p2222 amazi671@amazingcow.com
+}
+
+connect_helena()
+{
+    ssh n2omatt@192.168.0.120
+}
+
+connect_amanda()
+{
+    ssh mesquitax@192.168.0.121
+}
