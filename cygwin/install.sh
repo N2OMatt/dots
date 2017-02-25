e986c34af0838d2bb9d24071ae5396c49eac5414
n2omatt <n2o.matt@gmail.com>
2017-02-25 16:57:24 -0300

    - Update script to call the install_helper.

--- cygwin/install.sh
+++ cygwin/install.sh
@@ -21,47 +21,13 @@ LOCAL_FILES="files.sh";
 
 
 ################################################################################
-## Functions                                                                  ##
-################################################################################
-copy_files() {
-    FILES=$1;
-    DIR=$2;
-
-    for FILENAME in $FILES; do
-        FULLPATH=$DIR/$FILENAME;
-
-        #Sanity checking...
-        if [ ! -e $FULLPATH ]; then
-            echo "Error: ($FULLPATH) does not exists...";
-            exit 1;
-        fi;
-
-        #Copy...
-        echo "Copying ($FULLPATH) to ($OUTPUT_DIR)";
-        mkdir -p $OUTPUT_DIR;
-        cp $FULLPATH $OUTPUT_DIR;
-
-    done;
-}
-
-
-add_to_main() {
-    FILES=$1;
-
-    for FILENAME in $FILES; do
-        echo "Adding to main.sh: ($FILENAME)";
-        echo "source" "$DOTS_DIR/$FILENAME" >> "$OUTPUT_DIR/main.sh";
-    done;
-}
-
-
-################################################################################
 ## Script                                                                     ##
 ################################################################################
-##          Files          Files Directory.
-copy_files "$BASE_FILES"  "$BASE_DIR"
-copy_files "$LOCAL_FILES" "$LOCAL_DIR"
-
-
-add_to_main "$BASE_FILES"
-add_to_main "$LOCAL_FILES"
+echo "----> Calling dots install_helper.sh";
+./install_helper.sh             \
+    --basedir    "./base"       \
+    --localdir   "$OS"          \
+    --outputdir  "./output"     \
+    --dotsdir    "$DOTS_DIR"    \
+    --basefiles  "$BASE_FILES"  \
+    --localfiles "$LOCAL_FILES"
