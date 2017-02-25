ef0253de697d5c43537f1240e849ee0bcaa5db69
n2omatt <n2o.matt@gmail.com>
2017-02-25 16:59:20 -0300

    - Improve the script.

--- install.sh
+++ install.sh
@@ -1,43 +1,54 @@
 
-
-CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]");
+################################################################################
+## Variables                                                                  ##
+################################################################################
+CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]" | tr  "/" "_");
 BASHRC="$HOME/.bashrc";
 DOTS_DIR="$HOME/.${CURR_OS}_dots"
 BASHRC_ENTRY_FILENAME="$DOTS_DIR/main.sh"
 
 
+################################################################################
+## Script                                                                     ##
+################################################################################
+## Info
 echo "OS        : $CURR_OS";
 echo ".bashrc   : $BASHRC";
 echo ".dots dir : $DOTS_DIR";
 
-
+## Check .bashrc
 if [ ! -e "$BASHRC" ]; then
-    echo "No bashrc - exiting..."
+    echo "--> No bashrc - exiting..."
     exit 1;
 fi;
 
-
+## Check Previous installs.
 if [ -e "$DOTS_DIR" ]; then
-    echo "Found previous installation - Removing it...";
+    echo "--> Found previous installation - Removing it...";
     rm -rf "$DOTS_DIR";
 fi;
 
-
 ## Create dots dir and copy all files to it.
+echo "--> Creating the dots dir ($DOTS_DIR)";
 mkdir -p "$DOTS_DIR";
-./${CURR_OS}/install.sh base ${CURR_OS} output "$DOTS_DIR";
+
+echo "--> Calling $CURR_OS install script.";
+$CURR_OS/install.sh $CURR_OS $DOTS_DIR
+
+echo "--> Copying everything to dots dir ($DOTS_DIR)"
 cp -rf ./output/* "$DOTS_DIR";
 rm -rf output;
 
 
 ## Search on .bashrc if we already have an entry for dots
-## and if not create one!
+# ## and if not create one!
 GREP_RESULT=$(cat "$BASHRC" | grep "$BASHRC_ENTRY_FILENAME");
 if [ -z  "$GREP_RESULT" ]; then
-    echo "Did not found an entry in $BASHRC - Creating it...";
+    echo "--> Did not found an entry in $BASHRC - Creating it...";
     echo "[[ -s \"$BASHRC_ENTRY_FILENAME\" ]] && source \"$BASHRC_ENTRY_FILENAME\"" >> $BASHRC;
 else
-    echo "Found an entry in $BASHRC - Keeping it...";
+    echo "--> Found an entry in $BASHRC - Keeping it...";
 fi;
 
+echo "";
 echo "Installed...";
