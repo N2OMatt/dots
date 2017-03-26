9f8764e24ef70ad87e4b0e6c9317dbb5e9c04e39
n2omatt <n2o.matt@gmail.com>
2017-03-26 19:51:27 -0300

    - Improve the script.

--- gnu_linux/cocos.sh
+++ gnu_linux/cocos.sh
@@ -13,17 +13,26 @@ create-cocos-project()
     GAME_NAME="$1"
     GAME_NAME_LOWER=$(echo $1 | tr '[:upper:]' '[:lower:]');
 
+    if [ -z $GAME_NAME_LOWER ]; then
+        echo "[FATAL] Missing game name.";
+        return 1;
+    fi;
+
     PORTRAIT=$2
     #Portrait by default.
     if [ -z "$PORTRAIT" ]; then
         PORTRAIT="portrait";
     fi;
 
+    echo $GAME_NAME;
+    echo $GAME_NAME_LOWER;
+    echo $PORTRAIT;
+
     cocos new -p "com.amazingcow.$GAME_NAME_LOWER"           \
         -d .                                                 \
         --ios-bundleid "com.amazingcow.$GAME_NAME_LOWER.ios" \
         --mac-bundleid "com.amazingcow.$GAME_NAME_LOWER.mac" \
         -l cpp                                               \
-        $PORTRAIT                                            \
+        "--$PORTRAIT"                                        \
         $GAME_NAME
 }
