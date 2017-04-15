e54c6c4703766d805e196278828acceb2cf3a8e9
n2omatt <n2o.matt@gmail.com>
2017-04-14 21:49:57 -0300

    - Split the aliases logically and add more aliases.

--- base/git.sh
+++ base/git.sh
@@ -14,17 +14,25 @@
 ################################################################################
 #Less typing FTW.
 alias ggui="git gui";
-alias gtag="git tag";
+
 alias gstatus="git status -s";
 alias glog="git log";
+
 alias gadd="git add";
 alias gcommit="git commit";
 alias gpush="git push --tags";
 alias gpull="git pull";
+alias gtag="git tag";
+
 alias gsub-uir="git submodule update --init --recursive";
 alias gsub-add="git submodule add";
+
 alias gauthors="git log --format=\"%aN - <%ae>\" | sort | uniq";
 alias gauthors-top="git shortlog -s -n"
+
+alias gbranch="git branch";
+alias gbranch-curr="git rev-parse --abbrev-ref HEAD";
+
 
 
 ################################################################################
