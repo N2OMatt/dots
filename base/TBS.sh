ae8dbbf60ea72466ca0c9a804786c1a5bb2cc217
n2omatt <n2o.matt@gmail.com>
2017-06-08 17:32:14 -0300

    - Let us build whatever is the branch...

--- base/TBS.sh
+++ base/TBS.sh
@@ -14,12 +14,15 @@
 ################################################################################
 tbs-build()
 {
+    FORCE="$1";
     CURR_OS="";
+
     case $(uname -a | tr "[:upper:]" "[:lower:]") in
         *darwin* ) CURR_OS="mac";   ;;
         *cygwin* ) CURR_OS="win";   ;;
         *linux*  ) CURR_OS="linux"; ;; ## We actually don't use linux (yet...)
     esac
+
 
     IS_IN_SAGA_DIR=$(echo $PWD | grep "saga.-desktop/build");
     if [ -z "$IS_IN_SAGA_DIR" ]; then
@@ -27,14 +30,15 @@ tbs-build()
         return 1;
     fi;
 
+
     HG_BRANCH=$(hg branch);
     IS_ON_FH_BRANCH=$(echo $HG_BRANCH | grep "firehorse");
-    if [ -z "$IS_ON_FH_BRANCH" ]; then
+    if [ -z "$IS_ON_FH_BRANCH" ] && [ "$FORCE" != "--force" ]; then
         echo "Not in Firehorse branch ($HG_BRANCH). Aborting build.";
         return 1;
     fi;
 
-    SAGA_SKU=$(echo "$HG_BRANCH" | sed s/-firehorse/""/g);
+    SAGA_SKU=$(echo "$HG_BRANCH" | cut -d"-" -f1 );
     echo "Building $SAGA_SKU - For OS $CURR_OS";
     echo "   command: ant -f build-$SAGA_SKU.xml local info game-$CURR_OS-$SAGA_SKU";
 
