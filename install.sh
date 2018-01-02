1687964880840f7571f4d6e4f4234a93be58270b
n2omatt <n2omatt@amazingcow.com>
2018-01-02 02:29:50 -0200

    - Cosmetic changes.

--- install.sh
+++ install.sh
@@ -18,14 +18,14 @@
 ##---------------------------------------------------------------------------~##
 
 
-################################################################################
+##----------------------------------------------------------------------------##
 ## Variables                                                                  ##
-################################################################################
+##----------------------------------------------------------------------------##
 case $(uname -a | tr "[:upper:]" "[:lower:]") in
-    *darwin* ) CURR_OS="darwin";    ;;
-    *cygwin* ) CURR_OS="cygwin";    ;;
-    *linux*  ) CURR_OS="gnu_linux"; ;;
-    *freebsd* ) CURR_OS="bsd";      ;;
+    *darwin*  ) CURR_OS="darwin";    ;;
+    *cygwin*  ) CURR_OS="cygwin";    ;;
+    *linux*   ) CURR_OS="gnu_linux"; ;;
+    *freebsd* ) CURR_OS="bsd";       ;;
 esac
 
 BASHRC="$HOME/.bashrc";
@@ -33,26 +33,30 @@ DOTS_DIR="$HOME/.${CURR_OS}_dots"
 BASHRC_ENTRY_FILENAME="$DOTS_DIR/main.sh"
 
 
-################################################################################
+##----------------------------------------------------------------------------##
 ## Script                                                                     ##
-################################################################################
+##----------------------------------------------------------------------------##
+##------------------------------------------------------------------------------
 ## Info
 echo "OS        : $CURR_OS";
 echo ".bashrc   : $BASHRC";
 echo ".dots dir : $DOTS_DIR";
 
+##------------------------------------------------------------------------------
 ## Check .bashrc
 if [ ! -e "$BASHRC" ]; then
     echo "--> No bashrc - exiting..."
     exit 1;
 fi;
 
+##------------------------------------------------------------------------------
 ## Check Previous installs.
 if [ -e "$DOTS_DIR" ]; then
     echo "--> Found previous installation - Removing it...";
     rm -rf "$DOTS_DIR";
 fi;
 
+##------------------------------------------------------------------------------
 ## Create dots dir and copy all files to it.
 echo "--> Creating the dots dir ($DOTS_DIR)";
 mkdir -p "$DOTS_DIR";
@@ -64,9 +68,9 @@ echo "--> Copying everything to dots dir ($DOTS_DIR)"
 cp -rf ./output/* "$DOTS_DIR";
 rm -rf output;
 
-
+##------------------------------------------------------------------------------
 ## Search on .bashrc if we already have an entry for dots
-# ## and if not create one!
+## and if not create one!
 GREP_RESULT=$(cat "$BASHRC" | grep "$BASHRC_ENTRY_FILENAME");
 if [ -z  "$GREP_RESULT" ]; then
     echo "--> Did not found an entry in $BASHRC - Creating it...";
