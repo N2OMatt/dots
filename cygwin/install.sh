d3b1bc86295e6543bd2deff6ae50e39bb88e2308
n2omatt <n2omatt@amazingcow.com>
2018-01-02 02:30:08 -0200

    - Cosmetic changes and clean the install files.

--- cygwin/install.sh
+++ cygwin/install.sh
@@ -17,33 +17,29 @@
 ##    Installation script for Cygwin machines.                                ##
 ##---------------------------------------------------------------------------~##
 
-
-################################################################################
+##----------------------------------------------------------------------------##
 ## Variables                                                                  ##
-################################################################################
+##----------------------------------------------------------------------------##
 OS=$1;
 DOTS_DIR=$2
 
-
-BASE_FILES="AmazingCow.sh                \
-            bash-status-line_bindings.sh \
+BASE_FILES="bash-status-line_bindings.sh \
             coreutils.sh                 \
             development.sh               \
             git.sh                       \
             hacks.sh                     \
-            TBS.sh                       \
+            manual.sh                    \
             youtube-dl.sh                \
             ps1.sh";
 
 LOCAL_FILES="files.sh           \
              cygwin_hacks.sh    \
-             cygpath_helpers.sh \
-             firehorse_hacks.sh";
+             cygpath_helpers.sh";
 
 
-################################################################################
+##----------------------------------------------------------------------------##
 ## Script                                                                     ##
-################################################################################
+##----------------------------------------------------------------------------##
 echo "----> Calling dots install_helper.sh";
 echo "------> OS       : $OS";
 echo "------> DOTS_DIR : $DOTS_DIR";
@@ -57,12 +53,17 @@ echo "------> DOTS_DIR : $DOTS_DIR";
     --localfiles "$LOCAL_FILES"
 
 
+##------------------------------------------------------------------------------
+## Configure git.
+## COWTODO(n2omatt): check if this is still correct...
 echo "----> Configuring git...";
 git config --global core.preloadindex       true;
 git config --global core.fscache            true;
 git config --global gc.auto                 256;
 git config --global status.submoduleSummary false;
 
+
+##------------------------------------------------------------------------------
 ## COWNOTE(n2omatt): This is due cygwin don't provide
 ##    sudo command, and we want that GNU/Linux scripts
 ##    that require it works without modification.
