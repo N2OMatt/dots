e015621702453bc8848eb49b81ed2aa822b13c73
n2omatt <n2omatt@amazingcow.com>
2018-01-25 14:01:52 -0800

    - Simple way to get all #includes.

--- base/development.sh
+++ base/development.sh
@@ -12,3 +12,13 @@
 ## clang / g++ ##
 alias g++="g++ -std=c++11"       #C++11 by default.
 alias clang="clang++ -std=c++11" #C++11 by default.
+
+print_includes()
+{
+    START_PATH="$1";
+    if [ -z "$START_PATH" ]; then
+        START_PATH=".";
+    fi;
+
+    find $1 -name "*.h" -or -iname "*.cpp" | xargs grep -E '#include'
+}
